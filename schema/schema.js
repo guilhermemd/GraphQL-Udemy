f1704582b4f81eacb5a8b45a96b52c5aad11dfaa
GuilhermeMD <guilherme.md@gmail.com>
2021-11-28 19:21:35 -0300

    feat: :sparkles: schema - video 11

--- schema/schema.js
+++ schema/schema.js
@@ -13,3 +13,19 @@ const UserType = GraphQLObjectType({
     age: { type: GraphQLInt}
   }
 });
+
+// O RootQuery faz com que o GraphQL pule/vá para um especifico dado
+// Se der os 'args' eu retorno oo 'UserType'
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        
+      }
+    }
+  }
+})
